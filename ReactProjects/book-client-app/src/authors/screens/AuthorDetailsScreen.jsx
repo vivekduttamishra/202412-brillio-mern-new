657323aa528a27265afb3dcf1898d9d29267e607
Vivek Dutta Mishra <vivek@conceptarchitect.in>
2025-02-13 16:43:36 +0530

    day#36 update#3

--- ReactProjects/book-client-app/src/authors/screens/AuthorDetailsScreen.jsx
+++ ReactProjects/book-client-app/src/authors/screens/AuthorDetailsScreen.jsx
@@ -1,57 +1,33 @@
-import {useState,useEffect} from 'react'
-import {useParams} from 'react-router-dom'
-import Loading from '../../utils/components/Loading'
-import NotFound from '../../utils/components/NotFound';
+import { useParams } from 'react-router-dom'
 import { AuthorActions, getAuthorById } from '../../stores/author-store';
-import { useDispatch, useSelector } from 'react-redux';
-import { useStatus } from '../../stores/status-store';
+import { useSelector } from 'react-redux';
+import {AsyncContainer} from '../../utils/components/AsyncContainer';
+
+const AuthorDetailsScreen = () => {
+
+    const { id } = useParams();
+    const selectedAuthor = useSelector(s => s.selectedAuthor)
+
+    //getAuthorById(id)
 
 
-const AuthorDetailsScreen=()=>{
-
-    const {id}= useParams();
-    const selectedAuthor = useSelector(s=>s.selectedAuthor)
-    const status = useStatus(AuthorActions.AUTHOR_SELECT);
-
-    const dispatch = useDispatch();
-
-
-    useEffect(()=>{
-        getAuthorById(id)(dispatch);
-    },[id])    
-  
-
-    if(status.status==='pending')
-        return <Loading />;
-
-    else if(status.status==='error'){
-        // console.error('status.error',status.error);
-        // console.error('status.error.message',status.error.message);
-        // console.error('status.error.response', status.error.response);
-        if(status.error.message.includes('Network Error')){
-            return <h2>Network Error</h2>
-        } else if(status.error.response.status===404)
-            return <NotFound message={`Invalid Author: ${id}`}/>
-        else
-            return <h2>Unknown Error: {status.error.response.status}</h2>
-    }
-
-
-
-   
     return (
-        <div className="author-details-screen">
-            <h1>{selectedAuthor.name}</h1>   
-            <div className="row">
-                <div className="col col-4">
-                    <img src ={selectedAuthor.photo}/>
+        <AsyncContainer actionId={AuthorActions.AUTHOR_SELECT} 
+                        action = {()=>getAuthorById(id)} 
+        >
+            <div className="author-details-screen">
+                <h1>{selectedAuthor?.name}</h1>
+                <div className="row">
+                    <div className="col col-4">
+                        <img src={selectedAuthor?.photo} />
+                    </div>
+                    <div className="col col-8">
+                        <h2>Biography</h2>
+                        <p>{selectedAuthor?.biography}</p>
+                    </div>
                 </div>
-                <div className="col col-8">
-                    <h2>Biography</h2>
-                    <p>{selectedAuthor.biography}</p>
-                </div>
-            </div>         
-        </div>
+            </div>
+        </AsyncContainer>
     )
 }
 
--- ReactProjects/book-client-app/src/authors/screens/AuthorListScreen.jsx
+++ ReactProjects/book-client-app/src/authors/screens/AuthorListScreen.jsx
@@ -1,82 +1,38 @@
-import { useEffect } from 'react'
-import Loading from '../../utils/components/Loading';
 import { Link } from 'react-router-dom'
 import TitledComponent from '../../utils/components/TitledComponent';
-import withBorder from '../../utils/hoc/with-broder';
-
-import { useDispatch, useSelector } from 'react-redux';
-
-import AuthorService from '../services/AuthorService';
+import { useSelector } from 'react-redux';
 import { AuthorActions, getAllAuthors } from '../../stores/author-store';
-
-//let authorService = new AuthorService();
-// async function getAllAuthors(dispatch){
-//     //let dispatch = useDispatch();
-//     let authors=await authorService.getAll();
-//     dispatch({type:AuthorActions.AUTHORS, payload:authors});
-// }
+import {AsyncContainer} from '../../utils/components/AsyncContainer';
 
 
 const AuthorListScreen = () => {
 
     let authors = useSelector(s => s.authors);
-    let status = useSelector(s => s.status[AuthorActions.AUTHORS]);
-
-   // console.log('status', status);
-
-    let dispatch = useDispatch();
-
-    useEffect(() => {
-        // getAllAuthors(dispatch); //no need to await
-        getAllAuthors()(dispatch)
-
-    }, []);
-
-
-
-    if (!status || status.status === "pending")
-        return <Loading />
-
-    if (status.status === "error") {
-        console.error('error', status.error);
-        return (
-            <div>
-                <h2>Some Error Occured: {status.error.message}</h2>
-                <button 
-                onClick={()=>getAllAuthors()(dispatch)}
-                className="btn btn-primary">Retry</button>
-            </div>
-        )
-    }
-
-    if(status.status!=='success'){
-        return <div>Unknown Status: {status.status}</div>
-    }
-
+  
     return (
-        <TitledComponent title="Author List" expandable={true} >
-            <div className="author-list-screen">
+        <AsyncContainer actionId={AuthorActions.AUTHORS} 
+                        action={getAllAuthors}>
+            <TitledComponent title="Author List" expandable={true} >
+                <div className="author-list-screen">
 
-                <div className="row">
-                    {
-                        authors.map(author => (
-                            <div className=" author-card card col col-12 col-xl-2 col-lg-3 col-md-6 col-sm-6 col-xs-12"
-                                key={author.id}  >
-                                <img src={author.photo} className="card-img-top" alt={author.name} />
-                                <div className="card-body">
-                                    <h5 className="card-title">{author.name}</h5>
-                                    <p className="card-text">{author.biography?.substring(0, 50)}</p>
-
-                                    <Link to={`/authors/${author.id}`} className="btn btn-primary">Details</Link>
-
-
+                    <div className="row">
+                        {
+                            authors.map(author => (
+                                <div className=" author-card card col col-12 col-xl-2 col-lg-3 col-md-6 col-sm-6 col-xs-12"
+                                    key={author.id}  >
+                                    <img src={author.photo} className="card-img-top" alt={author.name} />
+                                    <div className="card-body">
+                                        <h5 className="card-title">{author.name}</h5>
+                                        <p className="card-text">{author.biography?.substring(0, 50)}</p>
+                                        <Link to={`/authors/${author.id}`} className="btn btn-primary">Details</Link>
+                                    </div>
                                 </div>
-                            </div>
-                        ))
-                    }
+                            ))
+                        }
+                    </div>
                 </div>
-            </div>
-        </TitledComponent>
+            </TitledComponent>
+        </AsyncContainer>
     )
 }
 
